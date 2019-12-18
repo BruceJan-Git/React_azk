9a5f58dbc15796dcd032a4e01fe1a49757e456aa
Bruce Jan <972063796@qq.com>
2019-12-18 16:57:59 +0800

    test antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import { Button } from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
 function App() {
   return (
     <div>
       <h1>React init</h1>
+      <Button type="primary">Click</Button>
     </div>
   );
 }
