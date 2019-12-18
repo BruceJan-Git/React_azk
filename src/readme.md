6251d756f58a75fc892d74849826dc8938552382
Bruce Jan <972063796@qq.com>
2019-12-18 20:13:19 +0800

    主页底部tab栏切换完成

---
+++ src/readme.md
@@ -0,0 +1,17 @@
+## web项目笔记
+
+## cli-init
+* npx create-react-app <file name>
+* yarn start
+* yarn add antd-mobile
+* import 'antd-mobile/dist/antd-mobile.css';
+* 配置全局默认样式,清除浏览器之间的css样式差异 index.css
+
+## 配置路由
+* 全局路由配置
+- yarn add react-router-dom
+- 默认Route配置(404页面配置)
+- 路由重定向配置
+
+* 配置主页面子路由
+- 路由链接跳转Link
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,33 +1,22 @@
 import React from 'react';
-
+import { TabBar } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+import './home.css'
 // 导入路由相关组件,配置主页面子路由
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-
-function Index() {
-  return (
-    <div>Index</div>
-  )
-}
-function Find() {
-  return (
-    <div>Find</div>
-  )
-}
-function Info() {
-  return (
-    <div>Info</div>
-  )
-}
-function My() {
-  return (
-    <div>My</div>
-  )
-}
+// import { BrowserRouter, Route, Link } from 'react-router-dom'
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'home',
+      hidden: false,
+      fullScreen: true,
+    };
+  }
   render() {
     return (
       <div>
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Route path='/home' component={Index}></Route>
           <Route path='/find' component={Find}></Route>
           <Route path='/info' component={Info}></Route>
@@ -36,9 +25,63 @@ class Home extends React.Component {
           <Link to='/find'>找房</Link>
           <Link to='/info'>资讯</Link>
           <Link to='/my'>我的</Link>
-        </BrowserRouter>
+        </BrowserRouter> */}
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          noRenderContent={true}
+          hidden={this.state.hidden}>
+            {this.renderMenuItem()}
+        </TabBar>
       </div>
     )
+  }
+
+  renderMenuItem = () => {
+    let mentData = [{
+      id: 'home',
+      title: '主页'
+    }, {
+      id: 'find',
+      title: '找房'
+    }, {
+      id: 'info',
+      title: '资讯'
+    }, {
+      id: 'my',
+      title: '我的'
+    }]
+    return mentData.map(item => {
+      return (
+        <TabBar.Item
+          icon={
+            <div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+            }}
+            />
+          }
+          selectedIcon={
+            <div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+            }}
+            />
+          }
+          title={item.title}
+          key={item.id}
+          selected={this.state.selectedTab === item.id}
+          onPress={() => {
+            this.setState({
+              selectedTab: item.id,
+            });
+          }}>
+        </TabBar.Item>
+      )
+    })
   }
 }
 
