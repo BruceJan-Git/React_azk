129801e15238959ea5ab9d89bfe5f87a419b12af
Bruce Jan <972063796@qq.com>
2019-12-22 17:54:44 +0800

    找房页面===顶部导航栏/icon未完成

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Login from './views/login/Login'
 import Home from './views/home/Home'
 import City from './views/city/City'
+import Test from './views/test/test'
 import '../src/utils/api' // 基准路径/响应拦截
 
 
@@ -21,6 +22,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path='/test' component={Test}></Route>
         <Route path='/login' component={Login}></Route>
         <Route path='/home' component={Home}></Route>
         <Route path='/city' component={City}></Route>
--- src/views/Test/test.js
+++ src/views/Test/test.js
@@ -1,2 +1,15 @@
-let arr = new Array(3)
-console.log(arr)
+import React from 'react';
+// require ('./font')
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <svg className="icon" aria-hidden="true">
+          <use xlinkHref="#icon-bofang"></use>
+        </svg>
+      </div >
+    )
+  }
+}
+export default Test
