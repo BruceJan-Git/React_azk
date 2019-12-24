bb4e723f6b4349352a46e0bb4c1ad2e8f46208cb
Bruce Jan <972063796@qq.com>
2019-12-24 13:59:05 +0800

    找房页面===条件筛选=>控制title以及弹窗是否有选值

--- src/views/find/components/Filter/index.js
+++ src/views/find/components/Filter/index.js
@@ -14,7 +14,7 @@ export default class Filter extends Component {
     super(props);
     this.state = {
       menuState: {
-        area: true,
+        area: false,
         mode: false,
         price: false,
         more: false
@@ -84,12 +84,47 @@ export default class Filter extends Component {
   componentDidMount() {
     this.loadFilterData()
   }
-  // 控制title选中与否
+  // 控制title以及弹窗是否有选值
   changeSelect = (props) => {
-    let menuState = { ...this.state.menuState }
-    menuState[props] = !menuState[props]
+    // let menuState = { ...this.state.menuState }
+    // menuState[props] = !menuState[props]
+    // this.setState({
+    //   menuState: menuState,
+    //   openType: props
+    // })
+    /**
+     * props是FilterTitle中传过来的类型
+     * 1. 控制菜单选中与否控制逻辑
+     * 2. 控制filterPicker中选项的选中与否
+     * 3. 当切换filterPiecker时只选中一个或者选中已经选中的title
+     */
+    let { menuState, menuValue } = { ...this.state }
+    let newMenuState = { ...this.state.menuState }
+    // 遍历菜单状态,取状态值从而进行判断
+    Object.keys(menuState).forEach(item => {
+      /**
+       * 获取对象的值
+       * menuValue定义的是一个对象,取值进行判断
+       * 根据menuValue的键名和menuState对象的键名一致
+       * 取处键名对应的键值(数组)的第一项,和默认值进行比较判断
+       */
+      let v = menuValue[item]
+      if (item === props) {
+        newMenuState[props] = true
+      } else if ((item === 'area' && v.length === 3) || v[0] === 'subway') {
+        newMenuState[item] = true
+      } else if (item === 'mode' && v[0] !== 'null') {
+        newMenuState[item] = true
+      } else if (item === 'price' && v[0] !== 'null') {
+        newMenuState[item] = true
+      } else if (item === 'more' && v.length !== 0) {
+        newMenuState[item] = true
+      } else {
+        newMenuState[item] = false
+      }
+    })
     this.setState({
-      menuState: menuState,
+      menuState: newMenuState,
       openType: props
     })
   }
